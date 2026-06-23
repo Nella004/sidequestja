aa9def5c96f5fbc3637df902d01b6927a0708aa4
Jhanel Lalor <lalorjhanel4@gmail.com>
2026-06-23 17:19:49 -0500

    Fix import path for useAuth in PageNotFound

--- src/lib/PageNotFound.jsx
+++ src/lib/PageNotFound.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from 'react-router-dom';
-import { useAuth } from '@/components/AuthContext.jsx';
+import { useAuth } from '@/lib/AuthContext.jsx';
 
 export default function PageNotFound() {
     const location = useLocation();
